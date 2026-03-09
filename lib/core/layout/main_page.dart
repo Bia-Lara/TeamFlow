ed341676e1b5034cd48b8a04f8144d36227b8301
Bia-Lara <agiampedro@gmail.com>
2026-03-09 17:05:33 -0300

    feat: menu e navegação entre paginas

---
+++ lib/core/layout/main_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import '../widgets/app_bottom_navbar.dart';
+
+import '../../features/home/presentation/pages/home_page.dart';
+import '../../features/groups/presentation/pages/groups_page.dart';
+import '../../features/profile/presentation/pages/profile_page.dart';
+import '../../features/tasks/presentation/pages/tasks_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  int currentIndex = 0;
+
+  final List<Widget> pages = [
+    const HomePage(),
+    const TasksPage(),
+    const GroupsPage(),
+    const ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      body: pages[currentIndex],
+
+      bottomNavigationBar: AppBottomNavbar(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
---
+++ lib/core/widgets/app_bottom_navbar.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class AppBottomNavbar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const AppBottomNavbar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      height: 80,
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+
+      decoration: const BoxDecoration(
+        color: Color(0xFF0B1026),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black54,
+            blurRadius: 20,
+            offset: Offset(0, -5),
+          )
+        ],
+      ),
+
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+
+          _buildItem(Icons.home_outlined, "Início", 0),
+
+          _buildItem(Icons.check_box_outlined, "Tarefas", 1),
+
+          _buildAddButton(),
+
+          _buildItem(Icons.groups_outlined, "Grupos", 2),
+
+          _buildItem(Icons.person_outline, "Perfil", 3),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildItem(IconData icon, String label, int index) {
+
+    final isSelected = currentIndex == index;
+
+    return GestureDetector(
+      onTap: () => onTap(index),
+
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+
+          Icon(
+            icon,
+            color: isSelected ? const Color(0xFF6C63FF) : Colors.white54,
+          ),
+
+          const SizedBox(height: 4),
+
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              color: isSelected ? const Color(0xFF6C63FF) : Colors.white54,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAddButton() {
+
+    return Container(
+      height: 60,
+      width: 60,
+
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        gradient: LinearGradient(
+          colors: [
+            Color(0xFF6C63FF),
+            Color(0xFF8F7BFF)
+          ],
+        ),
+      ),
+
+      child: const Icon(
+        Icons.add,
+        color: Colors.white,
+        size: 30,
+      ),
+    );
+  }
+}
