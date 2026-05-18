2f2a7516d2f3ed3d619ceef58e620d56994c3c07
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2026-05-17 22:41:53 -0300

    Group entity methods

--- lib/features/groups/data/group.entity.dart
+++ lib/features/groups/data/group.entity.dart
@@ -10,4 +10,27 @@ class Group {
     this.description,
     this.memberIds,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'description': description,
+      'memberIds': memberIds,
+    };
+  }
+
+  Group copyWith({
+    String? id,
+    String? name,
+    String? description,
+    List<String>? memberIds,
+  }) {
+    return Group(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      description: description ?? this.description,
+      memberIds: memberIds ?? this.memberIds,
+    );
+  }
 }
